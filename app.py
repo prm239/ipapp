88a0cb3af6f238d4976cb316f77b2e7ea7dc9a87
prm239 <prabhatranjanmisra@gmail.com>
2020-02-18 21:43:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import socket
 from flask import Flask,request
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
-	return request.remote_addr
-
+        host_name = socket.gethostname()
+        host_ip = socket.gethostbyname(host_name)
+        return host_ip
 app.run(host="0.0.0.0",port="8080")
